45e39b8bb1b8f568fb8a2950de2e52da28ad05f6
anajuarezar <ana.juarezar@gmail.com>
2021-04-05 00:09:06 -0500

    3rd commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,27 @@
+#We begin with the environment activated.
+# Then we import the modules we will be using to read de csv file. 
 import os
 import csv
 
+#We make a secure path for the file. 
+
 pybank_path = os.path.join('Resources', 'Lessons_03-Python_Homework_Instructions_PyBank_Resources_budget_data.csv')
 
-with open(pybank_path, newline='') as pybankfile:
-    reader = csv.reader(pybankfile, delimiter=",")
+#Then we bring the file and specify the delimitations. 
+
+with open(pybank_path) as cvsfile:
+    reader = csv.reader(cvsfile, delimiter=",")
+
+#we must separat the header so it will start in the next line
+    totalmonths = []
+
+    for row in reader:
+        month = row[0].split(" ")
+    
+    
+
+totalmonths = jan + feb + mar + abr + may + jun + july + august + sept + oct + nov + dec 
+
+print("Financial Analysis")
+print("--------------------------------------------")
+print(f"Total Months: {totalmonths}")
