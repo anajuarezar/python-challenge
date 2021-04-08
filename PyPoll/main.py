f7714fe445900d7104455cc3f8fac179f6692657
anajuarezar <ana.juarezar@gmail.com>
2021-04-07 19:27:46 -0500

    Pypoll done

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,34 +1,23 @@
+#We import the modules we will be using
 import os
 import csv
 
 
 poll_path = os.path.join('Resources', 'Lessons_03-Python_Homework_Instructions_PyPoll_Resources_election_data.csv')
-
-with open(poll_path, 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    first_row = next(reader)
-    for row in reader:
-        
-    
-
-
-with open(poll_path, 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    header = next(reader)
-    poll = {}
-    for row in reader:
-        names_dict = row[2]
-        poll[names_dict] = row[1:]
     
 
 with open(poll_path, 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     header = next(reader)
+    totalvotes = 0
+    candidates = []
     khan = []
     correy = []
     li = []
     tooley = []
     for row in reader:
+        totalvotes = totalvotes + 1
+        candidates.append(row[2])
         if row[2] == "Khan":
             khan.append(row[2])
         if row[2] == "Correy":
@@ -38,29 +27,19 @@ with open(poll_path, 'r') as csvfile:
         if row [2] == "O'Tooley":
             tooley.append(row[2])
     
+    def winner_cand(candidates):
+        return max(set(candidates), key = candidates.count)
+    
 
     votes_khan = len(list(khan))
     votes_correy = len(list(correy))
     votes_li = len(list(li))
     votes_tooley = len(list(tooley))
-    khan_percentage = (votes_khan/totalvotes)*100
-    correy_perc = (votes_correy/totalvotes)*100
-    li_perc = (votes_li/totalvotes)*100
-    tooley_perc = (votes_tooley/totalvotes)*100
-
-
-
-
-    
-print(dict.keys(poll))
-
-
-
-
-
-
-
-
+    khan_percentage =round((votes_khan/totalvotes)*100, 2)
+    correy_perc = round((votes_correy/totalvotes)*100, 2)
+    li_perc = round((votes_li/totalvotes)*100, 2)
+    tooley_perc = round((votes_tooley/totalvotes)*100, 2)
+    winner = max(candidates)
     
     
 
@@ -73,4 +52,5 @@ print(f"Khan: {khan_percentage}% ({votes_khan})")
 print(f"Correy: {correy_perc}% ({votes_correy})")
 print(f"Li: {li_perc}% ({votes_li})")
 print(f"O'Tooley: {tooley_perc}% ({votes_tooley})")
-
+print("--------------------------------")
+print(f"{winner_cand(candidates)}")
